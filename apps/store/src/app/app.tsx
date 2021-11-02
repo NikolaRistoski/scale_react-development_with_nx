de64f1cda6927c6850c520d97158568c1c057551
NikolaRistoski <nikolaristoski@pabau.com>
2021-11-02 16:46:33 +0100

    Install and use external packages, styling, shared componnets, feature libraries, generating backend api

---
+++ apps/store/src/app/app.tsx
@@ -0,0 +1,70 @@
+import './app.scss';
+
+import { getAllGames } from '../fake-api';
+
+// Shared component
+import { Header } from '@nxegghead/store/ui-shared';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import { formatRating } from '@nxegghead/store/util-formatters';
+
+import { Route, useHistory } from 'react-router-dom';
+
+import { StoreFeatureGameDetail } from '@nxegghead/store/feature-game-detail';
+
+export function App() {
+  const history = useHistory()
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="games-layout">
+          {getAllGames().map((x) => (
+            <Card
+              key={x.id}
+              className="game-card"
+              onClick={() => history.push(`/game/${x.id}`)}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className="game-card-media"
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className="game-rating"
+                  >
+                    <strong>Rating:</strong> {formatRating(x.rating)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
+      </div>
+            
+      <Route path="/game/:id" component={StoreFeatureGameDetail} />
+    </>
+  );
+}
+
+export default App;
